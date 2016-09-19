5e41d41e1f25da084a1b0b035d5616886a4a8748
Mir Ali <mir@alis-mac.local>
2016-09-19 19:00:51 +0530

    temp halt

---
+++ src/app/listing/listing.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ListingComponent } from './listing.component';
+
+describe('Component: Listing', () => {
+  it('should create an instance', () => {
+    let component = new ListingComponent();
+    expect(component).toBeTruthy();
+  });
+});

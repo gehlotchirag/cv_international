a193f17a52c0b6c3d60378f05139af20dd75cb4a
rupali <rupali.parab@craftsvilla.com>
2016-09-20 16:32:51 +0530

    product service - rupli

---
+++ src/app/product/product.ts
@@ -0,0 +1,11 @@
+export class Product{
+    constructor(
+    public productID: number,
+    public productName: string,
+    public productCode: string,
+    public releaseDate: string,
+    public price: string,
+    public description: string,
+    public ImageUrl: string
+    ){};
+}

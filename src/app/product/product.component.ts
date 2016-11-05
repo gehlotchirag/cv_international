64cccaba301b4027418e5839a24ab97fcf2d6b13
Mir Ali <mir.ali@craftsvilla.com>
2016-11-05 16:46:20 +0530

    master changes

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -79,8 +79,6 @@ export class ProductComponent implements OnInit {
     this.selectedSize="undefine";
     this.outOfStockMsgDspl=false;
     this.stitching_type_text_show=true;
-
-
   }
 
   onImageHover(imageSrc){
@@ -113,75 +111,74 @@ export class ProductComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.route
+        .data
+        .pluck('product', 'd')
+        .subscribe( (data: any) => {
+          this.products = data
+          this.productId=data.productId;
+          this.galleryImages=data.galleryImages;
+          // this.stitching_enabled=data.stitching_enabled;
+          this.stitching_type=data.stitching_type;
+          this.stitching_enabled=data.stitching_enabled;
+          this.imageUrl=data.imgUrl;
 
-    this.route.data.pluck('product', 'd').subscribe( (data: any) => {
-        this.products = data
-              this.productId=data.productId;
-              this.galleryImages=data.galleryImages;
-              // this.stitching_enabled=data.stitching_enabled;
-              this.stitching_type=data.stitching_type;
-              this.stitching_enabled=data.stitching_enabled;
-              this.imageUrl=data.imgUrl;
+          if(data.discountPercentage==0){
+              this.isDiscountPercVesible=false;
+          }
+          if(data.regularPrice==data.discountedPrice) {
+            this.isRegularPriceMore=false;
+          }
 
-              if(data.discountPercentage==0){
-                  this.isDiscountPercVesible=false;
-              }
-              if(data.regularPrice==data.discountedPrice) {
-                this.isRegularPriceMore=false;
-              }
+          if(this.stitching_enabled == false)
+          {
+            this.showSizeDiv=true;
+          }
 
-              if(this.stitching_enabled == false)
-              {
-                this.showSizeDiv=true;
-              }
+          if(data.isInStock <= 0)
+          {
+            this.outOfStockMsgDspl=true;
+          }
 
+          if(this.stitching_type=="")
+          {
+            this.stitching_type_text_show=false;
+          }
+          else if(this.stitching_enabled == true){
+            this.stitching_type_text_show=false;
+          }
+          else{
+              this.stitching_type_text_show=true;
+          }
+        },
+        (error) => console.error(error)
+      );
 
-              if(data.isInStock <= 0)
-              {
-                this.outOfStockMsgDspl=true;
-              }
-
-
-
-              if(this.stitching_type=="")
-              {
-                this.stitching_type_text_show=false;
-              }
-              else if(this.stitching_enabled == true){
-                this.stitching_type_text_show=false;
-              }
-              else{
-                  this.stitching_type_text_show=true;
-              }
-
-
-      },
-      (error) => console.error(error)
-      )
-
-     this.route.data.pluck('product','d', 'productAttributes').subscribe(
+   this.route
+        .data
+        .pluck('product','d', 'productAttributes')
+        .subscribe(
           (data: any) => {
             this.attributes = data
-
-
           },
           (error: any) => console.error(error),
           () => console.log('completed')
-        )
-      this.route.data.pluck('product','d', 'variants').subscribe(
-          (data: any) => {
-              this.sizes = data['Size']  || []
-              this.colors = data['Color'] || []
-              this.individualQuantity =data['Quantity'] || []
+        );
+
+    this.route.data.pluck('product','d', 'variants').subscribe(
+        (data: any) => {
+            this.sizes = data['Size']  || []
+            this.colors = data['Color'] || []
+            this.individualQuantity =data['Quantity'] || []
 
 
-              if(this.individualQuantity.length>0){
-                this.showSizeDiv=false;
-              }
-          },
-          (error: any) => console.error(error),
-          () => console.log('completed')
-      )
+            if(this.individualQuantity.length>0){
+              this.showSizeDiv=false;
+            }
+        },
+        (error: any) => console.error(error),
+        () => console.log('completed')
+    )
       this.route.data.pluck('product','d', 'chartList').subscribe(
             (data:any) => {
               this.columns = data['columns']
@@ -205,60 +202,35 @@ export class ProductComponent implements OnInit {
 
   }
 
-
-
-
-
-
-    addWish(event: any): void {
-
-        this.wishListService.addToWishList(this.customerId,this.productId );
-
+  addWish(event: any): void {
+      this.wishListService.addToWishList(this.customerId,this.productId );
     }
 
     addToBag(event: any): void {
-
-
-
-          if (typeof this.selectedSize == "undefine"){
-
-          this.addToBagUnSuccess=false;
-          this.addToBagSuccess=true;
-
-        }
-        else{
-          this.addToBagSuccess=false;
-          this.addToBagUnSuccess=true;
+      if (typeof this.selectedSize == "undefine"){
+        this.addToBagUnSuccess=false;
+        this.addToBagSuccess=true;
+      }
+      else{
+        this.addToBagSuccess=false;
+        this.addToBagUnSuccess=true;
         this.cartDetailsService.addToCart(this.customerId,this.productId,1)
-        }
-
-
+      }
     }
 
 
     selectSize(event:any, size: any): void{
-
       let index=this.individualQuantity.indexOf(size);
-
       this.selectedSize = (this.selectedSize === size) ? undefined : size;
-
     }
-
 
     showImage(event:any, galleryImage: any): void{
-
       let imageUrl=galleryImage;
-
       this.imageUrl=imageUrl;
-
     }
 
-
     selectStitchingType(stitchingType):void{
-
       this.stitchingType = stitchingType;
-
-
       if(this.stitchingType==='Readymade'){
         this.showSizeDiv=false;
       }else if(this.stitchingType==='Unstitched'){
@@ -267,17 +239,11 @@ export class ProductComponent implements OnInit {
       }else if(this.stitchingType==='Semi-stitched'){
         this.showSizeDiv=true;
         this.selectedSize="undefine";
-
       }else if(this.stitchingType==='Customized Stitch'){
-
         this.showSizeDiv=true;
         this.selectedSize="undefine";
-
       }
-
     }
-
-
 
   static reduceAttributes(prevItem, nextItem){
   	nextItem.forEach(function(item){

1aa64a785e471a40803c588c3d9f1e320a638c10
mohammedubaid <mohammedubaidc@gmail.com>
2016-12-21 20:17:21 +0530

    removed localstorage from megmenu

--- src/+app/shared/services/data-holding.service.ts
+++ src/+app/shared/services/data-holding.service.ts
@@ -7,21 +7,23 @@ export class DataHoldingService {
   private static hasLocalStorage: boolean = typeof window !== 'undefined' ? !!window.localStorage : false;
 
   static setItem(key: string, value: any) {
-    if(this.hasLocalStorage){
-        window.localStorage.setItem(String(key), JSON.stringify(value));
-    }
+    // window.localStorage.removeItem(String[key]);
+    // if(this.hasLocalStorage){
+    //     window.localStorage.setItem(String(key), JSON.stringify(value));
+    // }
     this.dataCache[String(key)] = value;
   }
 
   static getItem(key: string) {
-    let hasKey = false;
-    if(!this.dataCache[String(key)] && this.hasLocalStorage){
-      this.dataCache[String(key)] = JSON.parse(window.localStorage.getItem(String(key)));
-      return JSON.parse(window.localStorage.getItem(String(key)));
-    }
-    else{
+    // window.localStorage.removeItem(String[key]);
+    // let hasKey = false;
+    // if(!this.dataCache[String(key)] && this.hasLocalStorage){
+      // this.dataCache[String(key)] = JSON.parse(window.localStorage.getItem(String(key)));
+    //   return JSON.parse(window.localStorage.getItem(String(key)));
+    // }
+    // else{
       return this.dataCache[String(key)];
-    }
+    // }
   }
 
   static deleteItem(key: string){
